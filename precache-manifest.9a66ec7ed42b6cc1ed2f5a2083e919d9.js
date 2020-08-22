c8e0cf5f116aee7f42278fa9ac39f6f0c9dcdcc2
captain-tab <d491405879@gmail.com>
2020-08-22 18:43:57 +0800

    deploy

---
+++ precache-manifest.9a66ec7ed42b6cc1ed2f5a2083e919d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf30161ebb2a1c3875a5c23634a7ab0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d94050abbb4748ab689a",
+    "url": "./static/css/main.70d9c65a.chunk.css"
+  },
+  {
+    "revision": "32005d347bf7869ce3d8",
+    "url": "./static/js/2.4821bbb2.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "./static/js/2.4821bbb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94050abbb4748ab689a",
+    "url": "./static/js/main.ae184c5b.chunk.js"
+  },
+  {
+    "revision": "68816273c6e693b85416",
+    "url": "./static/js/runtime-main.2476c820.js"
+  }
+]);
